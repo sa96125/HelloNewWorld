764a6b0c10ffb496cea205918cc7fa43a7dd8ff6
sa96125 <sa96125@gmail.com>
2020-11-29 22:32:46 +0900

    2020-11-29(not finished yet)

---
+++ examples/react_web_movie/src/App3.js
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from "axios";
+
+class App3 extends React.Component {
+    // 사용할 데이터들을  욧다가 넣는 구만~
+    state = {
+        isLoading: true,
+        movies: []
+    };
+
+    //자바스크립트한테 이거 다운받을 때까지 기다려줘 왜냐면 이거 오래걸리니까.
+    //헤이 너 이거 언제 실행될지 모르겠는데 (비동기식)이 함수 실행 할 때 기다려야할 순간이 올거야. 괜찮??
+    //ㅇㅋ 근데 뭘? awit 이하를 실행할 때 잠깐 기다려줘
+    getMovies = async () => {
+        // movies.data.movies  객체로 받으니까 그안의 프롭 안에 프롭안에 값을 es6에서 이렇게 표현.
+        const { data: { movies } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+        this.setState({ movies, isLoading: false });
+    };
+
+    componentDidMount() {
+        // setTimeout(() => {
+        //     this.setState({ isLoading: false });
+        // }, 6000);
+        this.getMovies();
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        return <div> {isLoading ? "Loading ... " : "We are ready"} </div>
+    }
+}
+
+export default App3;
--- examples/react_web_movie/src/index.js
+++ examples/react_web_movie/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'; // 무조껀 선언해줘야하고
 import ReactDOM from 'react-dom';  // 리액트 js 루트에서 렌더링 할 폴더에 선언.
-import App2 from './App2';  // 같은 폴더 안에 있는 APPjs를 불러오는 선언
+import App3 from './App3';  // 같은 폴더 안에 있는 APPjs를 불러오는 선언
 
 ReactDOM.render(    // 리액트 어플리케이션은 1개의 콤포넌트만 렌더링한다!!!
   <React.StrictMode>
-    <App2 />
+    <App3 />
   </React.StrictMode>, // 콤포넌트는 HTML 를 반환하는 함수다!!! 제일중요, 콤포넌트 콤포노퉅 콘퍼논트!! JSX inly in React~
   document.getElementById('root') // root라는 이름을 가진 문서에다가 렌더링하겠다 이말임. App 콤포넌트를!!!!
 );
