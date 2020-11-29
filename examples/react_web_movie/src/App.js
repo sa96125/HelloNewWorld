61c35c0d0a730b35346a76c9b7393a19dc2507d1
sa96125 <sa96125@gmail.com>
2020-11-29 19:09:19 +0900

    2020-11-29

--- examples/react_web_movie/src/App.js
+++ examples/react_web_movie/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+const singerIlike = [
+  {
+    name:"iU",
+    song:"마시멜로우"
+  },
+  {
+    name:"하요시",
+    song:"하울의 움직이는 성"
+  },
+  {
+    name:"j park",
+    song:"sexy mommae"
+  },
+  {
+    name:"ssakthree",
+    song:"ggang"
+  },
+];
+
+//객체 통째로 전달하는 방법.
+function Moive(props) { 
+  console.log(props.actor);
+  return <h1>I love {props.actor}</h1>;
+}
+
+//내부 요소만 전달 ( NEW )
+function Singer({name}) { 
+  console.log({name});
+  return <h1>I love {name}</h1>;
+}
+
+//컴포넌트(함수)를 불러서 화면에 나타 나게 할 수 있다.
+//자바스크립트처럼 사용가능 {안에다가 코드}  
+//객체가 저장된 데이터타입에다가 콤포넌트의 인자로 객체 전달
+//Map 함수를 이용해서 return을 콤포넌트로 해버리면 안에 각 요소 전체에 접근해서 값을 전달받고 컴포넌트의 기능을 수행한다!!
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">  
+      {singerIlike.map(person =><Singer name={person.name} />)}    
+      <Moive name="바람과 함께 사라지다" actor={["장동건","고소영"]} />                   
+      <Singer name="GD 2020 콘서트" actor={["IU","GD"]} />                   
+  </div>       //콤포넌트로 데이터를 보낼 수도 있다. 
   );
 }
 
--- examples/react_web_movie/src/index.js
+++ examples/react_web_movie/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // 무조껀 선언해줘야하고
+import ReactDOM from 'react-dom';  // 리액트 js 루트에서 렌더링 할 폴더에 선언.
+import App from './App';  // 같은 폴더 안에 있는 APPjs를 불러오는 선언
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(    // 리액트 어플리케이션은 1개의 콤포넌트만 렌더링한다!!!
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode>, // 콤포넌트는 HTML 를 반환하는 함수다!!! 제일중요, 콤포넌트 콤포노퉅 콘퍼논트!! JSX inly in React~
+  document.getElementById('root') // root라는 이름을 가진 문서에다가 렌더링하겠다 이말임. App 콤포넌트를!!!!
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
