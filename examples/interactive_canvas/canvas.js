9209aeb08d196bc420becc2d01d8fcdeafdae4ca
sa96125 <sa96125@gmail.com>
2020-12-06 15:40:18 +0900

    2020-12-06(reviewed)

--- examples/interactive_canvas/canvas.js
+++ examples/interactive_canvas/canvas.js
@@ -2,7 +2,6 @@
 var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 var pen = canvas.getContext('2d');
 
 var mouse = {
@@ -46,7 +45,7 @@ function Circle(x, y, dx, dy, radius) {
         pen.beginPath();
         pen.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         // pen.strokeStyle = "orange";
-        pen.stroke();
+        // pen.stroke();
         pen.fillStyle = this.color;
         // pen.fillStyle = colorAraay[Math.floor(Math.random() * colorAraay.length)]
         pen.fill();
@@ -69,7 +68,6 @@ function Circle(x, y, dx, dy, radius) {
         } else if (this.radius > this.minradius) {
             this.radius -= 1;
         }
-        this.draw();
     }
 }
 
@@ -78,21 +76,23 @@ function animate() {
     pen.clearRect(0, 0, innerWidth, innerHeight);
 
     for (var i = 0; i < circleArray.length; i++) {
-        circleArray[i].update()
+        circleArray[i].draw();
+        circleArray[i].update();
     }
 }
 
 function init() {
     circleArray = [];
 
-    for (var i = 0; i < 200; i++) {
+    for (var i = 0; i < 100; i++) {
         var x = Math.random() * (innerWidth - (radius * 2)) + radius;
         var y = Math.random() * (innerHeight - (radius * 2)) + radius;
         var dx = (Math.random() - 0.5) * 5;
         var dy = (Math.random() - 0.5) * 5;
-        var radius = 15;
+        var radius = Math.random() * 10 + 10;
         circleArray.push(new Circle(x, y, dx, dy, radius));
     }
+
 }
 
 
