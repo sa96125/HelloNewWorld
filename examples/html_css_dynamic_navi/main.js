d8cf456ab7392a7027b04039eba95aec399df18a
sa96125 <sa96125@gmail.com>
2020-12-06 17:03:47 +0900

    2020-12-06(reviewed)

--- examples/html_css_dynamic_navi/main.js
+++ examples/html_css_dynamic_navi/main.js
@@ -1,6 +1,5 @@
 const toggleBtn = document.querySelector('.bar_togleBtn');
 const menu = document.querySelector('.bar_menu');
-const icons = document.querySelector('.bar_icons');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
