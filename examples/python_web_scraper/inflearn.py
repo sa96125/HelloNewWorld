b048f7c4a668bfde6f70c18fc12bd2213e35f6a7
sa96125 <sa96125@gmail.com>
2020-12-06 16:04:40 +0900

    2020-12-06(reviewed)

--- examples/python_web_scraper/inflearn.py
+++ examples/python_web_scraper/inflearn.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-LIMIT=1
-URL= f"https://www.inflearn.com/courses/it-programming?order=seq&page={LIMIT}"
+LIMIT = 1
+URL = f"https://www.inflearn.com/courses/it-programming?order=seq&page={LIMIT}"
+
 
 def extract_inflearn_pages():
     result = requests.get(URL)
@@ -10,26 +11,21 @@ def extract_inflearn_pages():
     pagination = soup.find("div", {"class": "pagination_container"})
     links = pagination.find_all("li")
 
-    pages=[]
+    pages = []
     for link in links:
         pages.append(link.find("a").string)
-        max_page=int(pages[-1])
+        max_page = int(pages[-1])
     return max_page
 
+
 def extract_inflearn_lectures(last_page):
-    lectures=[]
+    lectures = []
     for page in range(last_page):
-        result=requests.get(f"{URL}{page+1}")
+        result = requests.get(f"{URL}{page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
-        results = soup.find("div", {"class": "columns is-multiline is-mobile courses_card_list_body"})
-        titles = results.find("p").string
-        print(titles)
-    
+        results = soup.find(
+            "div", {"class": "columns is-multiline is-mobile courses_card_list_body"})
+        title = results.find("p").string
+        print(title)
+        lectures.append(title)
     return(lectures)
-
-
-# 느낀점:
-# 내가 자주 이용하는 인프런의 무료강의,유료강의 분리하고, 파이썬,go,등등 다양하게 스크래핑하고 싶어서
-# 10개 이상이 넘어가면 똑같은 자료가 반복되는 문제 때문에 하루종일 시간을 보냈는데..
-# 보안이 되어 있다는 걸 뒤늦게 알고 좌절. ㅠㅠ 이렇게도 가능하다라는 것만 보면 됐을껄.
-# 아무튼 간단한 코드로 이렇게 내가 원하는 데이터를 추출할 수 있다는 걸 알려준 과제.
