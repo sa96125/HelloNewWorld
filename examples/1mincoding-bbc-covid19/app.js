a2b5edc53e21d1d7534eaa034e4bcfb2cd86f1c3
sa96125 <sa96125@gmail.com>
2020-12-26 23:50:46 +0900

    2020-12-26

---
+++ examples/1mincoding-bbc-covid19/app.js
@@ -0,0 +1,27 @@
+(() => {
+    const stepElems = document.querySelectorAll('.step');
+    const graphElems = document.querySelectorAll('.graphic-item');
+
+
+    for (let i = 0; i < stepElems.length; i++) {
+        stepElems[i].dataset.index = i;
+        graphElems[i].dataset.index = i;
+    }
+
+    graphElems[0].classList.add('visible');
+
+    window.addEventListener('scroll', () => {
+        let step;
+        let boundingRect;
+
+        for (let i = 0; i < stepElems.length; i++) {
+            step = stepElems[i];
+            boundingRect = step.getBoundingClientRect();
+
+            if (boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8) {
+                graphElems[i].classList.add('visible');
+            }
+        }
+    })
+
+})();
