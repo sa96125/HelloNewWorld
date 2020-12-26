3400357dfce68ba47bbd3ea9371c16d0e0662298
sa96125 <sa96125@gmail.com>
2020-12-26 23:53:33 +0900

    2020-12-26(issue)

--- examples/1mincoding-bbc-covid19/app.js
+++ examples/1mincoding-bbc-covid19/app.js
@@ -2,7 +2,9 @@
     const stepElems = document.querySelectorAll('.step');
     const graphElems = document.querySelectorAll('.graphic-item');
 
-
+    //문제 해결해야 할 사항
+    //첫번째 화면은 계속 보이게 해야하고
+    //스크롤이벤트가 발생하면 해당사진 보이고 나머지는 삭제해야함!!
     for (let i = 0; i < stepElems.length; i++) {
         stepElems[i].dataset.index = i;
         graphElems[i].dataset.index = i;
