c344e24f0cb0f1d321cb6352fffd8d73ab9b87cc
sa96125 <sa96125@gmail.com>
2021-01-03 23:25:02 +0900

    2020-01-03

--- examples/interactive_kim_001/polygon.js
+++ examples/interactive_kim_001/polygon.js
@@ -1,6 +1,9 @@
-const PI2 = Math.PI * 2;
+const PI2 = Math.PI * 2; // 360도
 
+// 화면에 그려진 삼각형의 객체
 export class Polygon {
+
+    // x,y좌표, 반지름, 면의 갯수
     constructor(x, y, radius, sides) {
         this.x = x;
         this.y = y;
@@ -9,10 +12,11 @@ export class Polygon {
         this.rotate = 0;
     }
 
+    // 삼각형 그리기
     animate(ctx) {
         ctx.save();
-        ctx.fillStyle = '#000';
-        ctx.beginPath();
+        ctx.fillStyle = '#000'; // 채울 색갈
+        ctx.beginPath(); // 그림 시작
 
         const angle = PI2 / this.sides;
 
@@ -25,8 +29,8 @@ export class Polygon {
             (i == 0) ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
         }
 
-        ctx.fill();
-        ctx.closePath();
+        ctx.fill(); // 지정한 색으로 채우기
+        ctx.closePath(); // 그림 끝
         ctx.restore();
     }
 
