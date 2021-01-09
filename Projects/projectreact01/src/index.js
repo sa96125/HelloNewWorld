3ff34fab50ca93de1cc8223148ea503e661b5c24
sa96125 <sa96125@gmail.com>
2021-01-09 16:58:02 +0900

    2020-01-09(upload)

--- Projects/projectreact01/src/index.js
+++ Projects/projectreact01/src/index.js
@@ -1,17 +1,35 @@
+// 리액트 필요한 라이브러리 npm모듈에서 가져오기
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 컴포넌트(jsx)생성
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail
+                avatar={faker.image.cats()}
+                author={faker.name.firstName()}
+                timeAgo={faker.time.recent()}
+                content={faker.lorem.sentence()}
+            />
+            <CommentDetail
+                avatar={faker.image.people()}
+                author={faker.name.firstName()}
+                timeAgo={faker.time.recent()}
+                content={faker.lorem.sentence()}
+            />
+            <CommentDetail
+                avatar={faker.image.animals()}
+                author={faker.name.firstName()}
+                timeAgo={faker.time.recent()}
+                content={faker.lorem.sentence()}
+            />
+        </div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 컴포넌트 HTML에 보이기.
+ReactDOM.render(<App />, document.querySelector('#root'));
+
