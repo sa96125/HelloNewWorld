dd32b06840ba7900dbfaa911bf9ad2fa046cf2d4
sa96125 <sa96125@gmail.com>
2021-01-10 18:03:27 +0900

    2020-01-10

---
+++ Projects/projectreact02/src/SeasonDisplay.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+//
+const seasonConfig = {
+    summer: {
+        text: '대프리카에 오신 것은 환영합니다.',
+        iconName: 'sun'
+    },
+    winter: {
+        text: '스바 왜케 추운거여?',
+        iconName: 'snowflake'
+    }
+};
+
+// 현재 날짜와 위치를 동시에 확인해서 현재의 계절을 리턴해주는 함수.
+const getSeason = (lat, month) => {
+    if (month > 3 && month < 10) {
+        return lat > 35 ? 'summer' : 'winter';
+    } else {
+        return lat > 35 ? 'winter' : 'summer';
+    }
+}
+
+// 화면에 시즌을 표시하는 functional component.
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    // const text = season === 'winter' ? '스바 왜케 추운거여?' : '대프리카에 오신 것은 환영합니다.';
+    // const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const { text, iconName } = seasonConfig[season];
+    <i class="snowflake outline icon"></i>
+    return (
+        <div>
+            <i className={`massive ${iconName} outline icon`} />
+            <h1>{text}</h1>
+            <i className={`massive ${iconName} outline icon`} />
+        </div>
+    );
+};
+
+export default SeasonDisplay;
